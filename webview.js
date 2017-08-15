232b53d1d5527a3e862acd8d1ebc230e6aa14eef
Markus Spitzbarth <spitzbarth.markus@gmail.com>
2017-08-15 13:01:35 +0200

    added option for indirect messages

--- webview.js
+++ webview.js
@@ -1,17 +1,29 @@
 // Teamwork Chat integration
 module.exports = (Franz, options) => {
   function getMessages() {
-    let directCount = 0;
-    let count = 0;
-    // Get badge count from element
-    let messages = document.getElementsByClassName('sidebar-notification-indicator')[0];
+    const count = {};
 
-    if (messages && messages.innerText) {
-      count = parseInt(messages.innerText);
+    let directCount = 0;
+    let indirectCount = 0;
+
+    // Get badge count from direct messages
+    let directMessages = document.getElementsByClassName('sidebar-notification-indicator')[0];
+
+    if (directMessages && directMessages.innerText) {
+      directCount = parseInt(directMessages.innerText);
     }
 
+    // Get badge cound from indirect messages
+    var indirectMessages = document.getElementsByClassName("conversation-summary-unread-indicator--shown");
+    [].forEach.call(indirectMessages, function(e) {
+      var countValue = e.innerText;
+      if (countValue == "") {
+        ++indirectCount;
+      }
+    });
+
     // Set new count number for the badge
-    Franz.setBadge(count, messages);
+    Franz.setBadge(directCount, indirectCount);
   }
 
   // Runs action every second
